4ee3191c47a9301f3d14b4a7d21eadc964a2ee8d
Ricardo Marín Pérez <a01174384@tec.mx>
2024-08-26 16:18:06 -0600

    Agregar archivos de proyecto.

---
+++ Videos/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Videos
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            dataGridView1 = new DataGridView();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(550, 49);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Ejecutar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(273, 46);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(181, 49);
+            label1.Name = "label1";
+            label1.Size = new Size(71, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Parámetro 1";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(302, 176);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(336, 187);
+            dataGridView1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(181, 94);
+            label2.Name = "label2";
+            label2.Size = new Size(71, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Parámetro 2";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(273, 91);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(683, 213);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 38);
+            button2.TabIndex = 6;
+            button2.Text = "Descarga";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(dataGridView1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private Label label1;
+        private DataGridView dataGridView1;
+        private Label label2;
+        private TextBox textBox2;
+        private Button button2;
+    }
+}
---
+++ Videos/Form1.cs
@@ -0,0 +1,93 @@
+using System.Windows.Forms;
+using Videos.Algoritmos;
+
+namespace Videos{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            // Paso 0: Condición de vacío
+            if (textBox1.Text.Equals("") || textBox2.Text.Equals(""))
+            {
+                MessageBox.Show("Los números tienen que ser MAYOR que cero, NO VACÍOS");
+                return;
+            }
+
+            // Paso 1: Inicialización de Parámetros
+            int totalValores = Convert.ToInt32(textBox1.Text);
+            int valorMuestra = Convert.ToInt32(textBox2.Text);
+
+            // Paso 2: Declarar clase algortimo genético
+            AlgoritmoSimulacion algoritmo = new AlgoritmoSimulacion();
+
+            // Paso 3: Llamar método principal
+            List<int> listaEnteros = algoritmo.GenerarValores(totalValores);
+
+            // Paso 4: Llenar el Grid
+            llenarGrid(listaEnteros);
+        }
+
+        public void llenarGrid(List<int> lista)
+        {
+
+            // Paso 0: Indicar el número de columnas
+            string numeroColumna1 = "1";
+            string numeroColumna2 = "2";
+
+            // Paso 1: Determina la cantidad de columnas
+            dataGridView1.Columns.Clear();
+            dataGridView1.Columns.Add(numeroColumna1, "Id");
+            dataGridView1.Columns.Add(numeroColumna2, "Valor");
+
+            // Paso 2: Recorres el grid para cada fila llenas los valores aleatorios
+            for (int i = 0; i < lista.Count; i++)
+            {
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows[i].Cells[Int32.Parse(numeroColumna1) - 1].Value = (i + 1).ToString();
+                dataGridView1.Rows[i].Cells[Int32.Parse(numeroColumna2) - 1].Value = lista[i].ToString();
+            }
+
+        }
+
+        public void DescargaExcel(DataGridView data)
+        {
+            //Paso 0: Instalar complemento de Excel
+            Microsoft.Office.Interop.Excel.Application exportarExcel = new Microsoft.Office.Interop.Excel.Application();
+            exportarExcel.Application.Workbooks.Add(true);
+            int indiceColumna = 0;
+
+            //Paso 1: Construyes columnas y los nombres de las 'cabeceras'
+            foreach (DataGridViewColumn columna in data.Columns)
+            {
+                indiceColumna++;
+                exportarExcel.Cells[1, indiceColumna] = columna.HeaderText;
+            }
+
+            //Paso 2: Construyes filas y llenas valores
+            int indiceFilas = 0;
+            foreach (DataGridViewRow fila in data.Rows)
+            {
+                indiceFilas++;
+                indiceColumna = 0;
+                foreach (DataGridViewColumn columna in data.Columns)
+                {
+                    indiceColumna++;
+                    exportarExcel.Cells[indiceFilas + 1, indiceColumna] = fila.Cells[columna.Name].Value;
+                }
+            }
+
+            //Paso 3: Visibilidad
+            exportarExcel.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e){
+            DescargaExcel(dataGridView1);
+        }
+    }
+}
