c285498fc2e6c326f5c39a8232c22d7bb680f170
mzantana <zantana.miriam@gmail.com>
2024-08-26 18:10:22 -0600

    Modificacion Cuadrado medio

--- Videos/Algoritmos/AlgoritmoSimulacion.cs
+++ Videos/Algoritmos/AlgoritmoSimulacion.cs
@@ -22,14 +22,17 @@ namespace Videos.Algoritmos
         {
             List<int> listaSalida = new List<int>();
             int pseudoaleatorio = n;
-            n = n * n;
-            string cadena = n.ToString();
-            int longitud = cadena.Length;
-            int mitad = longitud / 2;
-            int inicio = mitad - 2;
-            string subcadena = cadena.Substring(inicio, 4);
-            pseudoaleatorio = Convert.ToInt32(subcadena);
-
+            while (!listaSalida.Contains(pseudoaleatorio))
+            {
+                listaSalida.Add(pseudoaleatorio);
+                int cuadrado = pseudoaleatorio * pseudoaleatorio;
+                string cadena = cuadrado.ToString();
+                int longitud = cadena.Length;
+                int mitad = longitud / 2;
+                int inicio = mitad - 2;
+                string subcadena = cadena.Substring(inicio, 4);
+                pseudoaleatorio = Convert.ToInt32(subcadena);
+            }
         }
     }
 }
